419406ad2c72a8ef53bbd3a7345e82f6869ac2f7
UClaudio <107913451+UClaudio@users.noreply.github.com>
2022-11-28 16:50:28 +0100

    first capstone commit

---
+++ src/main/java/com/claudiourru/capstone/entities/RoleType.java
@@ -0,0 +1,8 @@
+package com.claudiourru.capstone.entities;
+
+public enum RoleType {
+	ROLE_USER,
+	ROLE_DRIVER,
+	ROLE_RESTAURATEUR,
+	ROLE_ADMIN
+}
---
+++ src/main/java/com/claudiourru/capstone/entities/Status.java
@@ -0,0 +1,10 @@
+package com.claudiourru.capstone.entities;
+
+public enum Status {
+	
+	PENDING,
+	OPEN,
+	TAKEN_BY_DRIVER,
+	COMPLETED
+	
+}
